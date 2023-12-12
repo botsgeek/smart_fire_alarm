15c517857538d6c10290f3716f9aac5fdb77c9f8
RSaliu <ridwanriley@gmail.com>
2023-12-12 07:27:46 +0100

    [mq2-driver] - add initial mq2 driver source - init and deinit handling

---
+++ lib/mq2/src/mq2.c
@@ -0,0 +1,86 @@
+#include "mq2.h"
+#include <stdlib.h>
+#include "common_headers.h"
+#if defined(ARDUINO_AVR_NANO)
+#define MAX_ANALOG_PIN_NUMBER A7
+#define MIN_ANALOG_PIN_NUMBER A0
+#define MAX_DIGITAL_PIN_NUMBER 3
+#define MIN_DIGITAL_PIN_NUMBER 2
+#else
+#define MAX_ANALOG_PIN_NUMBER 10
+#define MIN_ANALOG_PIN_NUMBER 0
+#define MAX_DIGITAL_PIN_NUMBER 10
+#define MIN_DIGITAL_PIN_NUMBER 0
+#endif
+
+struct mq2_t {
+        uint8_t analog_pin_number;
+        uint8_t digital_pin_number;
+        mq2_mode_t mode;
+        bool initialized;
+    };
+
+static error_type_t mq2_init_analog_mode(mq2_t *mq2_object);
+static error_type_t mq2_init_digital_mode(mq2_t *mq2_object);
+
+mq2_t* mq2_create(const mq2_config config){
+    mq2_t* new_mq_2_obj = (mq2_t*) malloc(sizeof(mq2_t));
+    new_mq_2_obj->analog_pin_number = config.analog_pin_number;
+    new_mq_2_obj->digital_pin_number = config.digital_pin_number;
+    new_mq_2_obj->mode = config.mode;
+    return new_mq_2_obj;
+}
+
+static error_type_t mq2_init_analog_mode(mq2_t* mq2_object){
+    if(mq2_object->analog_pin_number <  MIN_ANALOG_PIN_NUMBER || mq2_object->analog_pin_number > MAX_ANALOG_PIN_NUMBER){
+            return INVALID_PIN_NUMBER;
+    }
+    mq2_object->initialized = true;
+    return OK;
+}
+
+static error_type_t mq2_init_digital_mode(mq2_t* mq2_object){
+    if(mq2_object->analog_pin_number <  MIN_DIGITAL_PIN_NUMBER || mq2_object->analog_pin_number > MAX_DIGITAL_PIN_NUMBER){
+            return INVALID_PIN_NUMBER;
+    }
+    pinMode(mq2_object->digital_pin_number,INPUT_PULLUP);
+    mq2_object->initialized = true;
+    return OK;
+}
+error_type_t mq2_init(mq2_t* mq2_object){
+    if(mq2_object->mode == MQ2_ANALOG_ONLY)
+    return mq2_init_analog_mode(mq2_object);
+    else if(mq2_object->mode == MQ2_DIGITAL_ONLY)
+    return mq2_init_digital_mode(mq2_object);
+    else if(mq2_object->mode == MQ2_HYBRID){
+        error_type_t analog_err = mq2_init_analog_mode(mq2_object);
+        if (analog_err != OK) return analog_err;
+        error_type_t digital_err = mq2_init_digital_mode(mq2_object);
+        if (digital_err != OK) return analog_err;
+        mq2_object->initialized = true;
+        return OK;
+    }
+    else return INVALID_PARAMETER;
+}
+
+error_type_t mq2_init_with_isr(mq2_t* mq2_object, void(*callback)()){
+    if(mq2_object->mode == MQ2_ANALOG_ONLY)return INVALID_PARAMETER;
+    error_type_t err = mq2_init(mq2_object);
+    if (err != OK)return err;
+    if(callback == NULL){
+        mq2_object->initialized = false;
+        return INVALID_PARAMETER;
+    }
+    attachInterrupt(digitalPinToInterrupt(mq2_object->digital_pin_number), callback, RISING);
+    mq2_object->initialized = true;
+    return OK;
+}
+
+error_type_t mq2_deinit(mq2_t* mq2_object){
+    // Since arduino does not de-initialized pins
+    // Nothing to do here than to free our object to
+    // Release the memory
+    free(mq2_object);
+    return OK;
+}
+
---
+++ lib/mq2/src/mq2.h
@@ -0,0 +1,72 @@
+#include <stdint.h>
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#elif defined(ARDUINO)
+#include <"WConstants.h">
+#else
+#include <stdio.h>
+#endif
+#ifndef _MQ2_H_
+#define _MQ2_H_
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include "common_headers.h"
+    typedef enum
+    {
+        MQ2_DIGITAL_ONLY,
+        MQ2_ANALOG_ONLY,
+        MQ2_HYBRID,
+
+    } mq2_mode_t;
+    /**
+     * @brief
+     *
+     */
+    typedef struct mq2_t mq2_t;
+    /**
+     * @brief
+     *
+     */
+    typedef struct
+    {
+        uint8_t analog_pin_number;
+        uint8_t digital_pin_number;
+        mq2_mode_t mode;
+    } mq2_config;
+
+    /**
+     * @brief
+     *
+     * @param mq2_object
+     * @return error_type_t
+     */
+    error_type_t mq2_init(mq2_t *mq2_object);
+    /**
+     * @brief
+     *
+     * @param mq2_object
+     * @param callback
+     * @return error_type_t
+     */
+    error_type_t mq2_init_with_isr(mq2_t *mq2_object, void (*callback)());
+    /**
+     * @brief
+     *
+     * @param mq2_object
+     * @return error_type_t
+     */
+    error_type_t mq2_deinit(mq2_t *mq2_object);
+    /**
+     * @brief
+     *
+     * @param config
+     * @return mq2_t*
+     */
+    mq2_t *mq2_create(const mq2_config config);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
