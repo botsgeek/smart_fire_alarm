65839dd595d96628f1546d779d7d65991ea585cb
oladeinde maryam <mariamoladeinde19@gmail.com>
2023-12-28 15:02:52 +0100

    declaration of pump_config_t, pump_init, pump_deinit and pump_create function

---
+++ lib/pump-driver/src/pump-driver.h
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#ifndef _PUMP_DRIVER_H_
+#define _PUMP_DRIVER_H_
+#ifdef __cplusplus
+extern "C"{
+#endif 
+#include "common_headers.h"
+
+typedef struct pump_t pump_t;
+
+typedef struct 
+{
+    uint8_t pump_pin_number;
+}pump_config_t;
+error_type_t pump_init(pump_t* pump_object);
+error_type_t pump_deinit(pump_t* pump_object);
+pump_t* pump_create(const pump_config_t* config);
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
