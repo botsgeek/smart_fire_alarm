fc23ee95c82fd574cf6d003c2ab47b72dd3b7bb1
oladeinde maryam <mariamoladeinde19@gmail.com>
2023-12-28 11:28:36 +0100

    fan-driver update

--- lib/fan-driver/src/fan-driver.c
+++ lib/fan-driver/src/fan-driver.c
@@ -16,13 +16,11 @@ struct fan_t
 
 fan_t* fan_create(const fan_config_t* config){
     if(config == NULL){
-        // Serial.println("cannot craete fan object from null config");
         return NULL;
     }
     fan_t* fan_obj= (fan_t*)malloc(sizeof(fan_t));
     fan_obj->fan_pin_number = config->fan_pin_number;
     fan_obj->activated = false;
-    //  Serial.println("/ fan_create is sucessful ");   
     return fan_obj; 
     
     
@@ -33,12 +31,11 @@ error_type_t fan_init(fan_t* fan_object){
     }
     if (fan_object->fan_pin_number != FAN_PIN_NUMBER)
     {
-        // Serial.println("cannot  return parameter from null config");
+       
         return INVALID_PIN_NUMBER;
     }
     pinMode(fan_object->fan_pin_number,OUTPUT);
-    fan_object->activated = true;
-    //   Serial.println("/ fan_init is sucessful ");  
+    fan_object->activated = true; 
     return OK; 
    
 }
@@ -47,7 +44,7 @@ error_type_t set_fanspeed(fan_t* fan_object,  uint8_t fanspeed){
     return NULL_PARAMETER;
     if (fan_object->activated != true)
     {
-        //  Serial.println("/ fan_ speed is sucessful ");
+      
         return INVALID_STATE;
     }
       
@@ -62,7 +59,7 @@ error_type_t deinit(fan_t* fan_object){
         free(fan_object);
        
     }
-    // Serial.println("/ fan_ deinit is sucessful ");
+   
     return OK;
 }
 
