9888480f01e234784ae33e7ce95c5dba6342c459
Dave-Olo <destineddave@gmail.com>
2024-01-09 17:40:45 +0100

    Struct Removed

--- lib/buzzer/buzzer.c
+++ lib/buzzer/buzzer.c
@@ -7,6 +7,8 @@
 #define MAX_BUZZER_PIN 13
 #define MIN_BUZZER_PIN 2
 
+
+
 //#define BUZZER_PIN 8 // Change this to your actual pin
  struct buzzer_t{
     uint8_t buzzer_pin;
@@ -21,8 +23,8 @@
 //     return buzzer_obj;
 // }
 
-   buzzer_t* buzzer_create(const buzzer_pin){
-    if(buzzer_pin== NULL) return NULL;
+    buzzer_t* buzzer_create(const uint8_t buzzer_pin){
+    // if(buzzer_pin == NULL) return NULL;
     buzzer_t* buzzer_obj = (buzzer_t*) malloc(sizeof(buzzer_t));
     buzzer_obj->buzzer_pin = buzzer_pin;
     buzzer_obj->activated = false;
@@ -45,6 +47,7 @@ error_type_t buzzer_deinit(buzzer_t* buzzer_object) {
         buzzer_object->activated = false;
         free(buzzer_object);
     }
+    return OK;
 }
 
 error_type_t buzzer_start(buzzer_t* buzzer_object, unsigned int pwm_duty_cycle) {
@@ -54,17 +57,14 @@ error_type_t buzzer_start(buzzer_t* buzzer_object, unsigned int pwm_duty_cycle) 
     if(buzzer_object->buzzer_pin <  MIN_BUZZER_PIN || buzzer_object->buzzer_pin > MAX_BUZZER_PIN){
             return INVALID_PIN_NUMBER;
     }
-    if(buzzer_object){
+    if(!buzzer_object-> activated){
         return INVALID_STATE;
 
     }
-    if (buzzer_object && buzzer_object->activated) {
+    
         analogWrite(buzzer_object->buzzer_pin,  pwm_duty_cycle);
         return OK;
-    }
-    else {
-        return FAILED;
-    }
+  
 }
 
 error_type_t buzzer_stop(buzzer_t* buzzer_object) {
@@ -74,15 +74,11 @@ error_type_t buzzer_stop(buzzer_t* buzzer_object) {
     if(buzzer_object->buzzer_pin <  MIN_BUZZER_PIN || buzzer_object->buzzer_pin > MAX_BUZZER_PIN){
             return INVALID_PIN_NUMBER;
     }
-    if(buzzer_object-> activated == false){
+    if(!buzzer_object-> activated){
         return INVALID_STATE;
 
     }
-    if (buzzer_object && buzzer_object->activated) {
+    
         analogWrite(buzzer_object->buzzer_pin, 0);
         return OK;
-    }
-    else {
-        return FAILED;
-    }
 }
