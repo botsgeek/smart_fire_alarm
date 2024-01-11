e481e5cb984a5ae31de34e8dd60121ff0f54da54
Dave-Olo <destineddave@gmail.com>
2024-01-11 08:10:11 +0100

    total_time parameter added

--- lib/buzzer/buzzer.h
+++ lib/buzzer/buzzer.h
@@ -14,11 +14,10 @@ extern "C"
 
     error_type_t buzzer_init(buzzer_t* buzzer_object);
     error_type_t buzzer_deinit(buzzer_t* buzzer_object);
-    error_type_t buzzer_start(buzzer_t* buzzer_object, unsigned int pwm_duty_cycle);
+    error_type_t buzzer_start(buzzer_t* buzzer_object, uint8_t pwm_duty_cycle);
     error_type_t buzzer_stop(buzzer_t* buzzer_object);
-    error_type_t buzzer_tone(buzzer_t* buzzer_object, unsigned int pwm_duty_cycle, unsigned int sound_duration, unsigned int silence_period);
+    error_type_t buzzer_tone(buzzer_t* buzzer_object, unsigned int sound_duration, unsigned int silence_period, unsigned long long total_time);
     buzzer_t* buzzer_create(const uint8_t buzzer_pin);
-  
 #ifdef __cplusplus
 }
 #endif
