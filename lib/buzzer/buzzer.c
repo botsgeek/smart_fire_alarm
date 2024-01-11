5f2475b17fd2298ed468193197907818642a9369
Dave-Olo <destineddave@gmail.com>
2024-01-11 15:15:41 +0100

    tone modified

--- lib/buzzer/buzzer.c
+++ lib/buzzer/buzzer.c
@@ -43,9 +43,7 @@ error_type_t buzzer_start(buzzer_t* buzzer_object, uint8_t pwm_duty_cycle) {
     if(buzzer_object == NULL){
         return NULL_PARAMETER;
     }
-    if(buzzer_object->buzzer_pin <  MIN_BUZZER_PIN || buzzer_object->buzzer_pin > MAX_BUZZER_PIN){
-            return INVALID_PIN_NUMBER;
-    }
+  
     if(!buzzer_object-> activated){
         return INVALID_STATE;
 
@@ -60,9 +58,7 @@ error_type_t buzzer_stop(buzzer_t* buzzer_object) {
     if(buzzer_object == NULL){
         return NULL_PARAMETER;
     }
-    if(buzzer_object->buzzer_pin <  MIN_BUZZER_PIN || buzzer_object->buzzer_pin > MAX_BUZZER_PIN){
-            return INVALID_PIN_NUMBER;
-    }
+    
     if(!buzzer_object-> activated){
         return INVALID_STATE;
 
@@ -73,35 +69,25 @@ error_type_t buzzer_stop(buzzer_t* buzzer_object) {
 }
 
 
-error_type_t buzzer_tone(buzzer_t* buzzer_object, unsigned int tone_duration, unsigned long long total_time) {
+error_type_t buzzer_tone(buzzer_t* buzzer_object, uint32_t tone_duration, uint32_t total_time) {
     if (buzzer_object == NULL) {
         return NULL_PARAMETER;
-    }
-    if (buzzer_object->buzzer_pin < MIN_BUZZER_PIN || buzzer_object->buzzer_pin > MAX_BUZZER_PIN) {
-        return INVALID_PIN_NUMBER;
     }
     if (!buzzer_object->activated) {
         return INVALID_STATE;
     }
 
-    unsigned long start_time = millis();
-    unsigned long current_time;
+    uint32_t start_time = millis();
+    uint32_t current_time;
     bool is_sound_active = false;
-    unsigned long sound_start_time = start_time;
-    unsigned long silence_start_time = start_time;
-
-    while ((current_time = millis()) - start_time < total_time) {
-        if ((is_sound_active ) && (current_time - sound_start_time >= tone_duration)) {
-            is_sound_active = false;
-            silence_start_time = current_time;
+    current_time = millis();
+    while (( millis() - start_time) < total_time) {
+        if (millis() - current_time >= tone_duration){
+            is_sound_active = !is_sound_active;
+            analogWrite(buzzer_object->buzzer_pin, is_sound_active ? pwm_duty_cycle : 0);
+            current_time = millis();
         }
-
-        if ((!is_sound_active) && (current_time - silence_start_time >= tone_duration)) {
-            is_sound_active = true;
-            sound_start_time = current_time;
-        }
-
-        analogWrite(buzzer_object->buzzer_pin, is_sound_active ? pwm_duty_cycle : 0);
+      
     }
 
     analogWrite(buzzer_object->buzzer_pin, 0);
